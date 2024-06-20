fc1fdca63fafa00a2e83972e48ca6ad7c4586dde
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2024-06-20 18:13:23 +0530

    server side data fetching

---
+++ app/fetch/page.tsx
@@ -0,0 +1,38 @@
+async function ServerSideFetch() {
+  const res = await  fetch('https://jsonplaceholder.typicode.com/users')
+
+  if(!res.ok) {
+    throw new Error('the data not fetch')
+  }
+  return res.json()
+}
+
+type Posts = {
+    name:string,
+    username:string,
+    id:number
+}
+
+
+async function FetchData() {
+   const data = await ServerSideFetch();
+
+   console.log(data);
+   
+  return (
+    <>
+    <div>FetchData</div>
+    {
+
+        data.map((post:Posts) => (
+            <div key={post.id}>
+                <h4>{post.name}</h4>
+                <h3>{post.username}</h3>
+            </div>
+       ))
+       }
+       </>
+  )
+}
+
+export default FetchData
