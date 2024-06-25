d9ab6514e5b6f4682e27475bc2fbd6621d7e97dc
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2024-06-25 17:58:14 +0530

    remove the error

--- components/PhotosCard.tsx
+++ components/PhotosCard.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { usePhoto } from '@/hooks/usePhoto';
+// import React from 'react';
+// import { usePhoto } from '@/hooks/usePhoto';
 
-type Photo = {
-  albumId: number;
-  id: number;
-  title: string;
-  url: string;
-  thumbnailUrl: string;
-}
+// type Photo = {
+//   albumId: number;
+//   id: number;
+//   title: string;
+//   url: string;
+//   thumbnailUrl: string;
+// }
 
-const PhotosCard = () => {
-  const { data: photos, error, isLoading } = usePhoto();
+// const PhotosCard = () => {
+//   const { data: photos, error, isLoading } = usePhoto();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
 
-  if (error) {
-    return <div>Error loading photos</div>;
-  }
+//   if (error) {
+//     return <div>Error loading photos</div>;
+//   }
 
-  return (
-    <div>
-      {photos && photos.map((photo: Photo) => (
-        <div key={photo.id}>
-          <h2>{photo.title}</h2>
-          <img src={photo.thumbnailUrl} alt={photo.title} />
-        </div>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {photos && photos.map((photo: Photo) => (
+//         <div key={photo.id}>
+//           <h2>{photo.title}</h2>
+//           <img src={photo.thumbnailUrl} alt={photo.title} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
-export default PhotosCard;
+// export default PhotosCard;
